e7450511d85cdc3cd8be72ec16a91ea839e5bc00
lorenzop <lorenzo@poixson.com>
2023-06-30 23:01:04 -0400

    added configs for noise parameters and added static defines for block types

--- src/com/poixson/backroomslite/Level0Generator.java
+++ src/com/poixson/backroomslite/Level0Generator.java
@@ -1,6 +1,9 @@
 package com.poixson.backroomslite;
 
+import static com.poixson.commonmc.utils.BlockUtils.StringToBlockData;
+
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.bukkit.Location;
@@ -14,6 +17,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.generator.WorldInfo;
 
+import com.poixson.tools.abstractions.AtomicDouble;
 import com.poixson.utils.FastNoiseLiteD;
 import com.poixson.utils.FastNoiseLiteD.CellularDistanceFunction;
 import com.poixson.utils.FastNoiseLiteD.CellularReturnType;
@@ -24,14 +28,33 @@ import com.poixson.utils.FastNoiseLiteD.RotationType3D;
 
 public class Level0Generator extends ChunkGenerator {
 
-	public static final double THRESH_WALL_L = 0.38;
-	public static final double THRESH_WALL_H = 0.5;
+	// default params
+	public static final double DEFAULT_NOISE_WALL_FREQ     = 0.022;
+	public static final int    DEFAULT_NOISE_WALL_OCTAVE   = 2;
+	public static final double DEFAULT_NOISE_WALL_GAIN     = 0.1;
+	public static final double DEFAULT_NOISE_WALL_LACUN    = 0.4;
+	public static final double DEFAULT_NOISE_WALL_STRENGTH = 2.28;
+	public static final double THRESH_WALL_L               = 0.38;
+	public static final double THRESH_WALL_H               = 0.5;
+
+	// default blocks
+	public static final String DEFAULT_BLOCK_WALL       = "minecraft:yellow_terracotta";
+	public static final String DEFAULT_BLOCK_CEILING    = "minecraft:smooth_stone_slab";
+	public static final String DEFAULT_BLOCK_SUBCEILING = "minecraft:stone";
+	public static final String DEFAULT_BLOCK_CARPET     = "minecraft:light_gray_wool";
 
 	protected final int level_y = 80;
 	protected final int level_h = 5;
 
 	// noise
 	protected final FastNoiseLiteD noiseLobbyWalls;
+
+	// params
+	public final AtomicDouble  noise_wall_freq     = new AtomicDouble( DEFAULT_NOISE_WALL_FREQ    );
+	public final AtomicInteger noise_wall_octave   = new AtomicInteger(DEFAULT_NOISE_WALL_OCTAVE  );
+	public final AtomicDouble  noise_wall_gain     = new AtomicDouble( DEFAULT_NOISE_WALL_GAIN    );
+	public final AtomicDouble  noise_wall_lacun    = new AtomicDouble( DEFAULT_NOISE_WALL_LACUN   );
+	public final AtomicDouble  noise_wall_strength = new AtomicDouble( DEFAULT_NOISE_WALL_STRENGTH);
 
 	// blocks
 	public final AtomicReference<String> block_wall       = new AtomicReference<String>(null);
@@ -44,13 +67,13 @@ public class Level0Generator extends ChunkGenerator {
 	public Level0Generator() {
 		// lobby walls
 		this.noiseLobbyWalls = new FastNoiseLiteD();
-		this.noiseLobbyWalls.setFrequency(0.022);
-		this.noiseLobbyWalls.setFractalOctaves(2);
-		this.noiseLobbyWalls.setFractalGain(0.1);
-		this.noiseLobbyWalls.setFractalLacunarity(0.4);
+		this.noiseLobbyWalls.setFrequency(              this.noise_wall_freq    .get());
+		this.noiseLobbyWalls.setFractalOctaves(         this.noise_wall_octave  .get());
+		this.noiseLobbyWalls.setFractalGain(            this.noise_wall_gain    .get());
+		this.noiseLobbyWalls.setFractalLacunarity(      this.noise_wall_lacun   .get());
+		this.noiseLobbyWalls.setFractalPingPongStrength(this.noise_wall_strength.get());
 		this.noiseLobbyWalls.setNoiseType(NoiseType.Cellular);
 		this.noiseLobbyWalls.setFractalType(FractalType.PingPong);
-		this.noiseLobbyWalls.setFractalPingPongStrength(2.28);
 		this.noiseLobbyWalls.setCellularDistanceFunction(CellularDistanceFunction.Manhattan);
 		this.noiseLobbyWalls.setCellularReturnType(CellularReturnType.Distance);
 		this.noiseLobbyWalls.setRotationType3D(RotationType3D.ImproveXYPlanes);
@@ -61,14 +84,14 @@ public class Level0Generator extends ChunkGenerator {
 	@Override
 	public void generateSurface(final WorldInfo worldInfo, final Random random,
 			final int chunkX, final int chunkZ, final ChunkData chunk) {
-		final Material block_wall       = Material.matchMaterial(this.block_wall      .get());
-		final Material block_subceiling = Material.matchMaterial(this.block_subceiling.get());
-		final Material block_carpet     = Material.matchMaterial(this.block_carpet    .get());
-		final Material block_ceiling    = Material.matchMaterial(this.block_ceiling   .get());
+		final BlockData block_wall       = StringToBlockData(this.block_wall,       DEFAULT_BLOCK_WALL      );
+		final BlockData block_ceiling    = StringToBlockData(this.block_ceiling,    DEFAULT_BLOCK_CEILING   );
+		final BlockData block_subceiling = StringToBlockData(this.block_subceiling, DEFAULT_BLOCK_SUBCEILING);
+		final BlockData block_carpet     = StringToBlockData(this.block_carpet,     DEFAULT_BLOCK_CARPET    );
 		if (block_wall       == null) throw new RuntimeException("Invalid block type for level 0 Wall"      );
+		if (block_ceiling    == null) throw new RuntimeException("Invalid block type for level 0 Ceiling"   );
 		if (block_subceiling == null) throw new RuntimeException("Invalid block type for level 0 SubCeiling");
 		if (block_carpet     == null) throw new RuntimeException("Invalid block type for level 0 Carpet"    );
-		if (block_ceiling    == null) throw new RuntimeException("Invalid block type for level 0 Ceiling"   );
 		double valueWall;
 		boolean isWall;
 		int xx, zz;
@@ -126,15 +149,34 @@ public class Level0Generator extends ChunkGenerator {
 
 
 	protected void loadConfig(final ConfigurationSection cfg) {
-		this.block_wall    .set(cfg.getString("Wall"    ));
-		this.block_carpet  .set(cfg.getString("Carpet"  ));
-		this.block_ceiling .set(cfg.getString("Ceiling" ));
+		// params
+		{
+			this.noise_wall_freq    .set(cfg.getDouble("Level0.Params.Noise-Wall-Freq"    ));
+			this.noise_wall_octave  .set(cfg.getInt(   "Level0.Params.Noise-Wall-Octave"  ));
+			this.noise_wall_gain    .set(cfg.getDouble("Level0.Params.Noise-Wall-Gain"    ));
+			this.noise_wall_lacun   .set(cfg.getDouble("Level0.Params.Noise-Wall-Lacun"   ));
+			this.noise_wall_strength.set(cfg.getDouble("Level0.Params.Noise-Wall-Strength"));
+		}
+		// block types
+		{
+			this.block_wall      .set(cfg.getString("Wall"      ));
+			this.block_ceiling   .set(cfg.getString("Ceiling"   ));
+			this.block_subceiling.set(cfg.getString("SubCeiling"));
+			this.block_carpet    .set(cfg.getString("Carpet"    ));
+		}
 	}
 	public static void ConfigDefaults(final FileConfiguration cfg) {
-		cfg.addDefault("Level0.Blocks.Wall",       "minecraft:yellow_terracotta");
-		cfg.addDefault("Level0.Blocks.SubCeiling", "minecraft:stone"            );
-		cfg.addDefault("Level0.Blocks.Carpet",     "minecraft:light_gray_wool"  );
-		cfg.addDefault("Level0.Blocks.Ceiling",    "minecraft:smooth_stone_slab");
+		// params
+		cfg.addDefault("Level0.Params.Noise-Wall-Freq",     DEFAULT_NOISE_WALL_FREQ    );
+		cfg.addDefault("Level0.Params.Noise-Wall-Octave",   DEFAULT_NOISE_WALL_OCTAVE  );
+		cfg.addDefault("Level0.Params.Noise-Wall-Gain",     DEFAULT_NOISE_WALL_GAIN    );
+		cfg.addDefault("Level0.Params.Noise-Wall-Lacun",    DEFAULT_NOISE_WALL_LACUN   );
+		cfg.addDefault("Level0.Params.Noise-Wall-Strength", DEFAULT_NOISE_WALL_STRENGTH);
+		// block types
+		cfg.addDefault("Level0.Blocks.Wall",       DEFAULT_BLOCK_WALL      );
+		cfg.addDefault("Level0.Blocks.Ceiling",    DEFAULT_BLOCK_CEILING   );
+		cfg.addDefault("Level0.Blocks.SubCeiling", DEFAULT_BLOCK_SUBCEILING);
+		cfg.addDefault("Level0.Blocks.Carpet",     DEFAULT_BLOCK_CARPET    );
 	}
 
 
